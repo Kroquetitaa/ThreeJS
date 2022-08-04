5055d3aa807af7aa51fb2e1c61da58472fc5ff4a
Kroquetitaa <Kroquetita@gmail.com>
2022-08-04 04:57:12 +0200

    Metodos para renderer y escena

--- src/js/app.js
+++ src/js/app.js
@@ -11,35 +11,27 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 );
-camera.position.set(0, 0, 10);
+camera.position.set(0, 0, 15);
 
 // Mesh
-const geometry = new THREE.BoxGeometry(2, 2, 2);
+const geometry = new THREE.BoxBufferGeometry(2, 2, 2);
 const material = new THREE.MeshBasicMaterial({
-    color: 'teal',
-    // wireframe: true,
-    // vertexColors: false,
-    // envMaps: 'refraction',
-    // map: 'bricks',
-    // refractionRadio: 1,
+    color: 'teal'
 });
-
 const box_mesh = new THREE.Mesh(geometry, material);
 scene.add(box_mesh);
 
-//camera.lookAt(box_mesh.position);
-
 // Render
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    canvas: container
+});
 renderer.setSize(container.clientWidth, container.clientHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
-
-container.appendChild(renderer.domElement);
 
 const update = () => {
     box_mesh.rotateX(0.01);
     box_mesh.rotateY(0.01);
-    // box_mesh.rotateZ(0.01);
     renderer.render(scene, camera);
     renderer.setAnimationLoop(() => update());
 }
